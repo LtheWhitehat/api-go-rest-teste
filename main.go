14d4bcbd26c205c58df0f27f3521dd013977a178
LtheWhitehat <lillian.moura@pagar.me>
2022-05-19 12:40:00 -0300

    remoção do mock de personalidades

--- main.go
+++ main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"api-go-rest-teste/DAO"
-	"api-go-rest-teste/models"
 	"api-go-rest-teste/rotas"
 	"fmt"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Lilly", Historia: "A hacker dos anos 00s"},
-		{Id: 2, Nome: "Joy", Historia: "A Professora revolucionária"},
-		{Id: 3, Nome: "Mori", Historia: "Bichinho Bichinhento"},
-	}
+
 	DAO.ConnectDB()
 	fmt.Println("Iniciando o servidor da API Rest...")
 	rotas.HandleRequest()
